5b0f8153be25f5b52f4168eab4d507cd7db9131d
artcrw <artemkropotin16@gmail.com>
2023-08-15 21:40:21 +0300

    перевод contract с unitest на py.test, реализация фикстуры

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,85 +1,27 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from contact import Contact
-
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/index.php")
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def fill_contect_form(self, wd, contact):
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-
-    def submit_contact_creation(self, wd):
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.return_to_home_page(wd)
+from contact_application import Capplication
 
 
-    def init_contact_creation(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def test_add_contact(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.init_contact_creation(wd)
-        self.fill_contect_form(wd, Contact(firstname="name", middlename="name2", lastname="name3", mobile="98394838498"))
-        self.submit_contact_creation(wd)
-        self.logout(wd)
-
-    def test_add_empty_contact(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.init_contact_creation(wd)
-        self.fill_contect_form(wd, Contact(firstname="", middlename="", lastname="", mobile=""))
-        self.submit_contact_creation(wd)
-        self.logout(wd)
+@pytest.fixture
+def app(request):
+    fixture = Capplication()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.init_contact_creation()
+    app.fill_contect_form(Contact(firstname="name", middlename="name2", lastname="name3", mobile="98394838498"))
+    app.submit_contact_creation()
+    app.logout()
 
-    
-    def tearDown(self):
-        self.wd.quit()
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_contact(app):
+    app.login(username="admin", password="secret")
+    app.init_contact_creation()
+    app.fill_contect_form(Contact(firstname="", middlename="", lastname="", mobile=""))
+    app.submit_contact_creation()
+    app.logout()
