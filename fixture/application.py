0d2d5190d8c4f255aa6b19d93b4c6566b9d53984
artcrw <artemkropotin16@gmail.com>
2023-09-13 11:04:51 +0300

    возможность выбора браузера

--- fixture/application.py
+++ fixture/application.py
@@ -5,12 +5,20 @@ from fixture.contact import ContactHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "safari":
+            self.wd = webdriver.Safari()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -22,7 +30,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (len(wd.find_elements_by_name("add")) > 0 and len(wd.find_elements_by_name("to_group")) > 0):
-            wd.get("http://localhost/addressbook/index.php")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
